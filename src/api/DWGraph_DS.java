95aa296745b2e16fa55aac8a69493f7c10f6b62d
Itay <itay.nextweb@gmail.com>
2020-12-03 16:43:19 +0200

    Fixed DWGraph_DS. Removed unnecessary comments.

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -15,7 +15,7 @@ public class DWGraph_DS implements directed_weighted_graph {
      * If node 3 has an edge TO node 5, then obtain it by: edge_data e = outEdges.get(3).get(5)
      * If node 5 has an edge FROM node 3, the obtain it by: edge_data e = inEdges.get(5).get(3) */
 
-    public DWGraph_DS() {
+    public DWGraph_DS() { //Empty constructor
         edgeSize = 0;
         countMC = 0;
         nodes = new HashMap<Integer, node_data>();
@@ -23,28 +23,25 @@ public class DWGraph_DS implements directed_weighted_graph {
         inEdges = new HashMap<Integer, HashMap<Integer, edge_data>>();
     }
 
-    public DWGraph_DS(directed_weighted_graph g) {
+    public DWGraph_DS(directed_weighted_graph g) { //Deep copy constructor
         if (g != null) {
             nodes = new HashMap<Integer, node_data>();
             outEdges = new HashMap<Integer, HashMap<Integer, edge_data>>();
             inEdges = new HashMap<Integer, HashMap<Integer, edge_data>>();
-            double weight;
+
             for (node_data v : g.getV()) {
-                node_data vCopy = new NodeData(v);
+                node_data vCopy = new NodeData(v); //Copy original node 'v' into 'vCopy'
                 nodes.put(vCopy.getKey(), vCopy);
-                HashMap<Integer, edge_data> outEdgeN = new HashMap<Integer, edge_data>(); // create vertex Neighbors HashMap.
-                HashMap<Integer, edge_data> inEdgeN = new HashMap<Integer, edge_data>(); // create vertex Neighbors HashMap.
-                outEdges.put(vCopy.getKey(), outEdgeN);
-                inEdges.put(vCopy.getKey(), inEdgeN);
-                for (edge_data e : g.getE(v.getKey())) { // run all of outgoing edges from v node's neighbors
-                    edge_data eCopy = new EdgeData(e);
-                    weight = g.getEdge(v.getKey(), e.getDest()).getWeight();
-                    outEdges.get(vCopy.getKey()).put(eCopy.getDest(), eCopy);
+                outEdges.put(vCopy.getKey(), new HashMap<Integer, edge_data>());
+                inEdges.put(vCopy.getKey(), new HashMap<Integer, edge_data>());
+
+                for (edge_data e : g.getE(v.getKey())) { //Iterate over outgoing edges from original node 'v'
+                    edge_data eCopy = new EdgeData(e); //Copy edge info
+                    outEdges.get(vCopy.getKey()).put(eCopy.getDest(), eCopy); //Update copied map
                 }
-                for (edge_data e : getInE(v.getKey())) { // run all of incoming edges from v node's neighbors
-                    edge_data eCopy = new EdgeData(e);
-                    weight = g.getEdge(e.getSrc(), v.getKey()).getWeight();
-                    inEdges.get(vCopy.getKey()).put(eCopy.getSrc(), eCopy);
+                for (edge_data e : getInE(v.getKey())) { //Iterate over incoming edges into original node 'v'
+                    edge_data eCopy = new EdgeData(e); //Copy edge info
+                    inEdges.get(vCopy.getKey()).put(eCopy.getSrc(), eCopy); //Update copied map
                 }
             }
             countMC = g.getMC();
@@ -109,7 +106,7 @@ public class DWGraph_DS implements directed_weighted_graph {
 
         if (!outEdges.get(src).containsKey(dest))  //If edge (src,dest) did not exist before, increment edgeSize.
             edgeSize++;
-        edge_data edge = new EdgeData(src, dest, w);
+        edge_data edge = new EdgeData(src, dest, w); //Generate a new edge from src to dest with weight 'w'.
         outEdges.get(src).put(dest, edge); //Put a new outgoing edge from src to dest
         inEdges.get(dest).put(src, edge); //Put a new incoming edge from dest to src
         countMC++;
@@ -155,30 +152,20 @@ public class DWGraph_DS implements directed_weighted_graph {
      */
     @Override
     public node_data removeNode(int key) {
-        if (nodes.containsKey(key)) { // if this graph contain this node
-//            node_data del = nodes.remove(key);
-//            int size = outEdges.get(key).size();
-//            size += inEdges.get(key).size();
-//            edgeSize -= size;
-//            outEdges.remove(key);
-//            inEdges.remove(key);
-//            return del;
-            Iterator<edge_data> itr = getE(key).iterator();
-            while(itr.hasNext()) { //Remove all outgoing edges from 'key'
-                edge_data e = itr.next(); //Hold the edge src --> dest(i)
-                removeEdge(e.getSrc(), e.getDest());
-//                inEdges.get(e.getDest()).remove(key);
-            }
+        if (nodes.containsKey(key)) return null;
 
-            itr = inEdges.get(key).values().iterator();
-            while (itr.hasNext()) { //Remove all incoming edges to 'key'
-                edge_data e = itr.next(); //Hold the edge from dest(i) --> src
-                removeEdge(e.getSrc(), e.getDest());
-//                outEdges.get(e.getSrc()).remove(key);
-            }
-            return nodes.remove(key);
+        Iterator<edge_data> itr = getE(key).iterator();
+        while (itr.hasNext()) { //Remove all outgoing edges from 'key'
+            edge_data e = itr.next(); //Hold the edge src --> dest(i)
+            removeEdge(e.getSrc(), e.getDest());
         }
-        return null;
+
+        itr = inEdges.get(key).values().iterator();
+        while (itr.hasNext()) { //Remove all incoming edges to 'key'
+            edge_data e = itr.next(); //Hold the edge from dest(i) --> src
+            removeEdge(e.getSrc(), e.getDest());
+        }
+        return nodes.remove(key);
     }
 
     /**
